93b1a0643f2d2a9f9af0c26cee21ea5193704732
Mister-Oatbread <david.hafe82@gmail.com>
2025-11-16 19:04:51 +0100

    several changes, it is now possible to move a chip around

--- main.lua
+++ main.lua
@@ -8,6 +8,9 @@ function _init()
     x_zero_pos = 128;
     y_zero_pos = 128;
 
+    x_zero_pos = 0;
+    y_zero_pos = 0;
+
     -- initialize empty field
     for col = 1,columns do
         board[col] = {}
@@ -16,54 +19,107 @@ function _init()
         end
     end
 
+    player_1_to_move = true;
     -- initialize players
     player_1 = {
         token_sprite = 33,
-        to_move = true,
+        cursor_pos = 4,
+        left_button = 0,
+        right_button = 1,
     };
 
     player_2 = {
         token_sprite = 35,
-        to_move = false,
+        cursor_pos = 4,
+        left_button = 4,
+        right_button = 5,
     };
+
+    active_token = {
+        x_pos = 0,
+        y_pos = 0,
+        sprite = player_1.token_sprite,
+    };
+
+    -- redefine button press refresh rate
+    poke(0x5f5c, 255);
+
+    _set_fps(15);
 end
 
 function _update()
+    update_active_chip();
 end
 
 function _draw()
     cls();
-    camera(128,128);
+    camera(x_zero_pos, y_zero_pos);
     map();
     spr(33, 184, 240, 2, 2);
     spr(35, 136, 160, 2, 2);
 
-    print(get_user_input());
+    spr(active_token.sprite, active_token.x_pos, active_token.y_pos, 2,2);
+end
+
+-- determines where the cursor is currently located
+function update_active_chip()
+    input = get_user_input()
+
+    if (player_1_to_move) then
+        player = player_1;
+    else
+        player = player_2;
+    end
+
+    if (input == "right") then
+        player.cursor_pos += 1;
+    elseif (input == "left") then
+        player.cursor_pos -= 1;
+    end
+
+    if (player.cursor_pos == 0) then
+        player.cursor_pos = 7;
+    elseif (player.cursor_pos == 8) then
+        player.cursor_pos = 1;
+    end
+
+    -- write active token state to cursor
+    x_pos, y_pos = calculate_coords_from_field(player.cursor_pos,1);
+    active_token.x_pos = x_pos;
+    active_token.y_pos = y_zero_pos + 4;
+    active_token.sprite = player.token_sprite;
+
 end
 
 -- takes in board position and calculates pixel position for sprite
 function calculate_coords_from_field(row, column)
     x_coord = 16*row + x_zero_pos - 8;
     y_coord = 16*column + y_zero_pos + 16;
-    return {x_coord, y_coord};
+    return x_coord, y_coord;
+end
+
+-- this function takes in a column and returns the first free entry
+function send_token_down(column)
+    while field
+        board[column]
 end
 
 -- this function registers what the user is doing
 -- only takes inputs from active player
 function get_user_input()
 
-    if (player_1.to_move) then
-        player = 1;
-    elseif (player_2.to_move) then
-        player = 2;
+    if (player_1_to_move) then
+        player = player_1;
     else
-        print("it seems like it is neither player's turn");
+        player = player_2;
     end
 
-    if (btnp(1, player)) then
+    if (btnp(player.right_button)) then
         return "right";
-    elseif (btnp(0, player)) then
+    elseif (btnp(player.left_button)) then
         return "left";
+    elseif (btn(player.left_button) and btn(player.right_button)) then
+        return "place";
     else
         return "idle";
     end
